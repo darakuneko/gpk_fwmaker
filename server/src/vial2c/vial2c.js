d8ef7c832a3de549f17a837756ae4f1bf6e1c17b
Daraku-Neko <tvkids@gmail.com>
2024-10-16 00:46:19 +0900

    fix: KC_ALIASES

--- server/src/vial2c/vial2c.js
+++ server/src/vial2c/vial2c.js
@@ -129,7 +129,7 @@ const vial2c = (json) => {
     return ""; 
   }
 
-  const translateKeycode = (keycode) =>  keycode.replace(/LT([0-9])\((KC_[A-Z]+)\)/g,  (_, p1, p2) => `LT(${p1}, ${KC_ALIASES[p2] || p2})`)
+  const translateKeycode = (keycode) =>  keycode.replace(/LT([0-9]+)\((KC_[A-Z]+)\)/g,  (_, p1, p2) => `LT(${p1}, ${KC_ALIASES[p2] || p2})`)
 
   const keymap = json.layout
     .map((layout, layoutIndex) => {
